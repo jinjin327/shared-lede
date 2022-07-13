04a24b577d3baed48b8370782afacb72342974a0
jinjin327 <32284406+jinjin327@users.noreply.github.com>
2022-07-13 19:49:36 +0800

    Update 0modify.sh

--- 0modify.sh
+++ 0modify.sh
@@ -1,2 +1,2 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
