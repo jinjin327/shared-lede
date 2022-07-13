4b698f8b39232d3d8b62ffd846082aabf4bf490d
jinjin327 <32284406+jinjin327@users.noreply.github.com>
2022-07-13 10:12:04 +0800

    Update 0clone.sh

--- 0clone.sh
+++ 0clone.sh
@@ -2,7 +2,7 @@
 #新增机型预留脚本
 
 # 下载源码
-git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/immortalwrt/immortalwrt
-mv ./immortalwrt/* ./
+git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/padavanonly/immortalwrtmt7622
+mv ./immortalwrtmt7622/* ./
 
 # 下载自定义插件
